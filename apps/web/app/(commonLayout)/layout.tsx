f11a3cde70465f8849d79362c2c3dd54794fb998
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-06-16 18:44:20 +0600

    add: project setup layout

---
+++ apps/web/app/(commonLayout)/layout.tsx
@@ -0,0 +1,13 @@
+
+
+
+const CommonLayout = ({children}:any) => {
+    return (
+        <>
+       {/* <Navbar/> */}
+        {children} 
+        </>
+    )
+}
+
+export default CommonLayout;
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,30 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
+import type { Metadata } from "next";
 import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import { Toaster } from "@workspace/ui/components/sonner";
+import "./font.css"; 
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
 
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+export const metadata : Metadata = {
+  title: "AMDADUL HQ | Portfolio",
+  description: "Creative Web Developer",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
+    <html lang="en">
+      <body suppressHydrationWarning className={`font-[Montreal] antialiased`}>
+          <Toaster richColors position="bottom-right" />
+          {children}
       </body>
     </html>
-  )
+  );
 }
