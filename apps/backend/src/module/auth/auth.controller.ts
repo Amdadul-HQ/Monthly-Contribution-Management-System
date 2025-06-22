ea6fb42b5d66739a89e9e238acd9a7bf06e2585d
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-06-23 00:14:52 +0600

    add: user info update

--- apps/backend/src/module/auth/auth.controller.ts
+++ apps/backend/src/module/auth/auth.controller.ts
@@ -1,9 +1,37 @@
 import { catchAsync } from "../../app/helper/catchAsync";
+import { AuthService } from "./auth.service";
 
 const signup = catchAsync(async(req,res) => {
     
-    const memberData = req.body;
-    const result = 
+    const {name,fatherName,motherName,address,occupation,email,phone,nid,joingDate,refrenceName,montlyDeposit,registrationFee,paymentMethod,password} = req.body;
+
+    const personalInfo = {
+        name,
+        fatherName,
+        motherName,
+        address,
+        occupation,
+        email,
+        phone,
+        nid,
+        joingDate,
+        refrenceName,
+    }
+
+    const personalInfoStates = {
+        montlyDeposit,
+        registrationFee,
+        paymentMethod
+    }
+
+    const credentials = {
+        name,
+        email,
+        phone,
+        password,
+    }
+
+    const result = await AuthService.signup()
 })
 
 
