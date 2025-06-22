7f3d006bf4c9ad424b930406431659032ca3fdfd
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-06-22 20:03:05 +0600

    add: prisma migrate

---
+++ apps/backend/src/module/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { catchAsync } from "../../app/helper/catchAsync";
+
+const signup = catchAsync(async(req,res) => {
+    
+    const memberData = req.body;
+    const result = 
+})
+
+
+export const AuthController = {
+    signup
+}
---
+++ apps/backend/src/module/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import prisma from "../../app/shared/prisma"
+
+const signup = async (data) => {
+    const result = await prisma.
+}
+
+export const AuthService =  {
+    signup
+}
