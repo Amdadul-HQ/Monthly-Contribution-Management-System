dfe4395ff9877c6d79f32e808c06b4d1b3c066d0
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-06-23 17:45:06 +0600

    api implement in service

--- apps/web/service/authService/index.ts
+++ apps/web/service/authService/index.ts
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
 
 export const signUpMember = async (memberData: FieldValues) => {
@@ -19,6 +20,36 @@ export const signUpMember = async (memberData: FieldValues) => {
     return result;
   } catch (error: any) {
     console.error("Register error:", error);
+    return { success: false, message: error.message };
+  }
+};
+
+export const login = async (data: FieldValues) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
+
+    const result = await res.json();
+
+    if (result.success && result?.data?.accessToken) {
+      const cookieStore = await cookies();
+      cookieStore.set("accessToken", result.data.accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        path: "/",
+        sameSite: "lax",
+      });
+    }
+
+    return result;
+  } catch (error: any) {
+    console.error("Login error:", error);
     return { success: false, message: error.message };
   }
 };
