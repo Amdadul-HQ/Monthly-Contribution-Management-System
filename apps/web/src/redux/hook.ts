051b2aa1a7300ce5fd4fca0cf1f4868dc321141a
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-23 09:43:16 +0600

    feat: add authentication API and Redux slice for user management, including login, user info retrieval, and password reset functionalities

--- apps/web/src/redux/hook.ts
+++ apps/web/src/redux/hook.ts
@@ -1,6 +1,7 @@
-import { useDispatch, useSelector, useStore } from "react-redux";
-import { AppDispatch, AppStore, RootState } from "./store";
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import type { AppDispatch, RootState } from './store'
 
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
